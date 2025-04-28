d7676505d0d714b7d777475b7afc4cc7cf110191
Inprodi Web Studio <proyectos@inprodi.mx>
2025-04-28 14:01:18 -0600

    validate duplicated rfc

--- src/api/fiscal/controllers/fiscal.js
+++ src/api/fiscal/controllers/fiscal.js
@@ -1,12 +1,30 @@
-const { FISCAL } = require('../../../constants/models');
+const { FISCAL } = require("../../../constants/models");
+const { BadRequestError } = require("../../../helpers/errors");
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController(FISCAL, ({strapi}) => ({
     async create(ctx) {
         const data = ctx.request.body;
 
-        const fiscal = await strapi.entityService.create( FISCAL, { data });
+        const { user } = ctx.state;
+
+        const existing = await strapi.entityService.findMany(FISCAL, {
+            filters: {
+                rfc: data.rfc,
+                user: user.id,
+            },
+            limit: 1,
+        });
+
+        if (existing && existing.length > 0) {
+            throw new BadRequestError("Duplicated rfc", {
+                key: "fiscal.duplicatedRfc",
+                path: ctx.request.path,
+            });
+        }
+
+        const fiscal = await strapi.entityService.create(FISCAL, { data });
 
         return fiscal;
     },
