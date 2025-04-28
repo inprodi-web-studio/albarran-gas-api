a1d64d18577bfb17f2f60977cb91e0adb8008430
Inprodi Web Studio <proyectos@inprodi.mx>
2025-04-28 12:12:57 -0600

    fiscal create route

--- src/api/fiscal/controllers/fiscal.js
+++ src/api/fiscal/controllers/fiscal.js
@@ -1,9 +1,13 @@
-'use strict';
-
-/**
- * fiscal controller
- */
+const { FISCAL } = require('../../../constants/models');
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::fiscal.fiscal');
+module.exports = createCoreController(FISCAL, ({strapi}) => ({
+    async create(ctx) {
+        const { data } = ctx.request.body;
+
+        const fiscal = await strapi.entityService.create( FISCAL, { data });
+
+        return fiscal;
+    },
+}));
